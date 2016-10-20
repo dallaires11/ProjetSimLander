313a85a576ca2380054dad6015e776a65a3e78ee
Sam Dallaire <waterfalldallaire@gmail.com>
2016-10-20 13:49:53 -0400

    Vrai base de projet JavaFx

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-package sample;
-
-import javafx.application.Application;s
+import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,7 +7,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Group root = new Group;
+        Group root = new Group();
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
