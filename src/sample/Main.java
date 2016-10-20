56fe47a09227295fd4530d65a21c554c89526d57
Sam Dallaire <waterfalldallaire@gmail.com>
2016-10-20 13:46:00 -0400

    Structure de base

---
+++ src/sample/Main.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.application.Application;s
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Group root = new Group;
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
