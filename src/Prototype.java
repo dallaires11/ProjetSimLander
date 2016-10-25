8d0797ef68134bfc6f2b80718087dd9011be3b9b
Sam Dallaire <waterfalldallaire@gmail.com>
2016-10-25 17:11:20 -0400

    +Mouvement haut bat +Variable de Rotation +Entre SPACE RIGHT LEFT

--- src/Prototype.java
+++ src/Prototype.java
@@ -5,29 +5,57 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.security.Key;
 
 public class Prototype extends Application {
 
     Vaisseu vaisseu = new Vaisseu();
     Rectangle vesseau = new Rectangle(650,100,20,20);
+    boolean accelerationJoueur=false;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Group root = new Group();
-        Label vitesse = new Label("vitesse :" + vaisseu.getVitesse());
-        Label position = new Label("Position :" + vaisseu.getVitesse());
+        Label vitesse = new Label("vitesseY :" + vaisseu.getVitesseY());
+        Label position = new Label("Position :" + vesseau.getTranslateY());
+        Label acY = new Label("AccY: "+(vaisseu.getAccelationy()-0.0098));
+        Label rotation = new Label("Rotation:"+vaisseu.getRotation());
+        position.setTranslateY(10);
+        acY.setTranslateY(20);
+        rotation.setTranslateY(30);
 
 
-        root.getChildren().addAll(vesseau, vitesse);
+        root.getChildren().addAll(vesseau,vitesse,position,acY,rotation);
 
+        Scene cena = new Scene(root, 1400, 700);
+        cena.setOnKeyPressed(event -> {
+            if(event.getCode()== KeyCode.SPACE)
+                accelerationJoueur=true;
+            else if(event.getCode()== KeyCode.LEFT){
+                vaisseu.rotationGauche();
+            }
+            else if(event.getCode()==KeyCode.RIGHT){
+                vaisseu.rotationDroite();
+            }
+        });
+        cena.setOnKeyReleased(event -> {
+            if(event.getCode()== KeyCode.SPACE)
+                accelerationJoueur=false;
+
+        });
         Timeline gravite=new Timeline(new KeyFrame(
                 Duration.millis(15), t->{
-            vaisseu.accelerer();
-            vesseau.setTranslateY(vesseau.getTranslateY()+(vaisseu.getVitesse()));
-            vitesse.setText("vitesse :" + vaisseu.getVitesse());
+            vaisseu.accelerer(accelerationJoueur);
+            vesseau.setTranslateY(vesseau.getTranslateY()+vaisseu.getVitesseY());
+            //vesseau.setTranslateX(vesseau.getTranslateX()+vaisseu.getVitesseX());
+            vitesse.setText("vitesse :" + vaisseu.getVitesseY());
             position.setText("Position :" + vesseau.getTranslateY());
+            acY.setText("AccY: "+(vaisseu.getAccelationy()-0.0098));
+            rotation.setText("Rotation:"+vaisseu.getRotation());
             //System.out.println((int)(vaisseu.getVitesse()));
         }
         ));
@@ -35,7 +63,7 @@ public class Prototype extends Application {
         gravite.play();
 
         primaryStage.setTitle("SimLander - Prototype");
-        primaryStage.setScene(new Scene(root, 1400, 700));
+        primaryStage.setScene(cena);
         primaryStage.show();
     }
 
--- src/Vaisseu.java
+++ src/Vaisseu.java
@@ -2,16 +2,67 @@
  * Created by Chroon on 2016-10-25.
  */
 public class Vaisseu {
-    float vitesse;
+    float vitessey;
+    float vitessex;
+    int rotation;
+    float accelationx=0;
+    float accelationy=0;
     public Vaisseu(){
-        vitesse= 0.00f;
+        vitessey= 0.00f;
+        vitessex= 0.00f;
+        rotation=90;
+        accelationx=0;
+        accelationy=0;
     }
 
-    public float getVitesse(){
-        return  vitesse;
+
+
+    public float getVitesseY(){
+        return  vitessey;
     }
 
-    public  void accelerer(){
-        vitesse+=0.0098f;
+    public float getVitesseX() {
+        return vitessex;
+    }
+
+    public float getAccelationy() {
+        return accelationy;
+    }
+
+    public float getAccelationx() {
+        return accelationx;
+    }
+
+
+    public  void accelerer(boolean accelerationJoueur){
+        System.out.println(accelerationJoueur);
+        if(accelerationJoueur) {
+            System.out.println("test");
+            //accelationx=(float)(0.02f*Math.cos(rotation));
+            accelationy = (float) (0.05f * Math.sin(rotation));
+        }
+        else  if(!accelerationJoueur)
+            accelationy=0;
+
+        vitessey+=0.0098f-accelationy;
+        //vitessex+=accelationx;
+    }
+
+    public int getRotation(){
+        return  rotation;
+    }
+
+    public void rotationDroite(){
+        if (rotation==0)
+            rotation=359;
+        else
+            rotation--;
+    }
+
+    public void rotationGauche(){
+        if(rotation==359)
+            rotation=0;
+        else
+            rotation++;
     }
 }
