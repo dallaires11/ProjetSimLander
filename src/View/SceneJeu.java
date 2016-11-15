28b034922574fe64c673ad75e414e3b3c876037d
Sam Dallaire <waterfalldallaire@gmail.com>
2016-11-15 15:37:33 -0500

    Tres peite changement

--- src/View/SceneJeu.java
+++ src/View/SceneJeu.java
@@ -3,6 +3,40 @@
  */
 package View;
 
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+
 public class SceneJeu {
+    private Scene jeu;
+
+    public SceneJeu(){
+        Group root=new Group();
+
+
+
+
+
+
+
+        jeu=new Scene(root,1400,700);
+    }
+
+    private void setAction(){
+        jeu.setOnKeyPressed(event -> {
+            public void handle();
+            public 
+        });
+        jeu.setOnKeyReleased(event -> {
+
+        });
+    }
+
+
+
+
+
+
 
 }
