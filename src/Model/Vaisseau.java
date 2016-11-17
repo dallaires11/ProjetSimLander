fa65a85a574d2a5c6a8170ad619bb908bae87eb7
Vincent <vincent@boily.ca>
2016-11-17 14:17:07 -0500

    vaisseau et classe perdre

--- src/Model/Vaisseau.java
+++ src/Model/Vaisseau.java
@@ -17,7 +17,7 @@ public class Vaisseau extends Group {
     private int positionX, positionY;
     private Label nom;
 
-    public Vaisseau(){
+    public Vaisseau() {
         essence = 1000;
         vitesseX = 0;
         vitesseY = 0;
@@ -29,47 +29,51 @@ public class Vaisseau extends Group {
         this.getChildren().addAll(new VBox(nom, generateImageVaisseau()));
     }
 
-    private Group generateImageVaisseau(){
-        Polygon triangle = new Polygon(0,20, 10,0, 20,20);
-        Rectangle gun1 = new Rectangle(5,15, 2,15);
-        Rectangle gun2 = new Rectangle(15,15, 2,15);
+    private Group generateImageVaisseau() {
+        Polygon triangle = new Polygon(0, 20, 10, 0, 20, 20);
+        Rectangle gun1 = new Rectangle(5, 15, 2, -15);
+        Rectangle gun2 = new Rectangle(15, 15, 2, -15);
 
         return new Group(triangle, gun1, gun2);
     }
 
-    public void reduireEssence(int reduction){
+    public void reduireEssence(int reduction) {
         essence -= reduction;
     }
 
-    public void rotationGauche(int vitesseRotation){
+    public void rotationGauche(int vitesseRotation) {
         rotation -= vitesseRotation;
         if (rotation < 0)
             rotation = 360 + rotation;
     }
 
-    public void rotationDroite(int vitesseRotation){
-        rotation =+ vitesseRotation;
+    public void rotationDroite(int vitesseRotation) {
+        rotation = +vitesseRotation;
         if (rotation > 360)
             rotation -= 360;
     }
 
-    private void bouger(){
+    private void bouger() {
         positionX += vitesseX;
         positionY += vitesseY;
     }
 
-    public void accelerer(boolean appuyer){
+    public void accelerer(boolean appuyer) {
         double accelerationX = 0;
         double accelerationY = 0;
-        if(appuyer){
-            accelerationX = 10 * Math.cos((double)rotation);
-            accelerationY = 10 * Math.sin((double)rotation);
+        if (appuyer) {
+
+            accelerationX = 10 * Math.cos((double) rotation);
+            accelerationY = 10 * Math.sin((double) rotation);
             reduireEssence(10);
         }
         vitesseX += accelerationX;
         vitesseY += accelerationY - 9.8;
         bouger();
+        accelerationX = 6 * Math.cos((double) rotation);
+        accelerationY = 6 * Math.sin((double) rotation);
     }
+
 
     public int getEssence(){
         return essence;
@@ -93,6 +97,7 @@ public class Vaisseau extends Group {
         } else
             nom.setText(nouveauNom);
     }
-
-
 }
+
+
+
--- src/View/Perdre.java
+++ src/View/Perdre.java
@@ -3,5 +3,45 @@
  */
 package View;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+
 public class Perdre {
+    static Scene perdre;
+    Group groupe;
+
+
+
+
+    public Perdre(){
+        Rectangle background = new Rectangle(1401,701, Color.BLACK);
+
+        Text texte = new Text(null);
+        Font amatic = Font.loadFont("AmaticSc-Regular.ttf", 60);
+
+        texte.setFont(amatic);
+        //texte.setScaleX(); texte.setScaleY();
+        texte.setFill(Color.CRIMSON);
+        texte.setText("VOUS AVEZ PERDU");
+
+        groupe = new Group(background, texte);
+        perdre = new Scene(groupe, 1400, 700);
+    }
+
+    public static void setScene(Stage stage){
+        stage.setScene(perdre);
+
+    }
+
+    public Group getGroup(){
+        return groupe;
+    }
 }
