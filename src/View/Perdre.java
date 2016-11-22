c1da34ed2b1f897f43f1c426ba2639f83c801ce8
Vincent <vincent@boily.ca>
2016-11-22 17:12:19 -0500

    Scene perdre terminee, Menu.getSceneMenu() mis static

--- src/View/Perdre.java
+++ src/View/Perdre.java
@@ -3,6 +3,8 @@
  */
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -10,35 +12,52 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.awt.*;
+import javafx.scene.control.Button;
 import java.io.File;
 
 public class Perdre {
     static Scene perdre;
     Group groupe;
-
-
-
+    Stage ceStage;
 
     public Perdre(){
         Rectangle background = new Rectangle(1401,701, Color.BLACK);
 
         Text texte = new Text(null);
-        Font amatic = Font.loadFont("AmaticSc-Regular.ttf", 60);
+        Text lol = new Text("Get good.");
 
-        texte.setFont(amatic);
-        //texte.setScaleX(); texte.setScaleY();
-        texte.setFill(Color.CRIMSON);
-        texte.setText("VOUS AVEZ PERDU");
 
-        groupe = new Group(background, texte);
+        texte.setScaleX(10); texte.setScaleY(10);
+        texte.setFill(Color.RED);
+        texte.setText("YOU HAVE CRASHED");
+        texte.setLayoutY(300);
+        texte.setLayoutX(650);
+
+        lol.setLayoutY(400);
+        lol.setLayoutX(650);
+        lol.setFill(Color.WHITE);
+        lol.setScaleX(1.2); lol.setScaleY(1.2);
+
+        Button toMenu = new Button("I suck, take me back home");
+        toMenu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ceStage.setScene(Menu.getSceneMenu());
+            }
+        });
+
+
+        groupe = new Group(background, texte, lol, toMenu);
         perdre = new Scene(groupe, 1400, 700);
     }
 
-    public static void setScene(Stage stage){
+    public void setScene(Stage stage){
         stage.setScene(perdre);
+        ceStage = stage;
+    }
 
+    public static Scene getScene(){
+        return perdre;
     }
 
     public Group getGroup(){
